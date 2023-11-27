bd3ac1486c856b04eb394d91903a49fbced32929
SauloMFreitas <103659497+SauloMFreitas@users.noreply.github.com>
2023-11-27 13:18:04 -0300

    mudanças gerais

--- graph/GraphMatrix.hpp
+++ graph/GraphMatrix.hpp
@@ -2,18 +2,43 @@
 #include <vector>
 #include <climits>
 #include <stack>
-
+#include <map>  
+#include <set>
+#include <algorithm>
 
 using namespace std;    
+
 
 class GraphMatrix
 {
 private:
-    vector<vector<int>> adjMatrix;
-    int numVertices;
-    bool _hasEdge(const int &from, const int &to);
-    bool dfs(int v, vector<bool>& visitado, vector<bool>& pilhaRecursiva, vector<int>& parent, stack<int>& ciclo);
 
+    // ----- estrutura para armazenar as informações das arestas
+    struct Edge
+    {
+        int source, target, score;
+        vector<Edge> kicksOut;
+        Edge(int s, int t, int score):source(s), target(t), score(score){}
+        Edge() : source(-1), target(-1), score(INT_MAX){}
+    };
+
+    // ----- variáveis privadas
+
+    vector<vector<Edge>> adjMatrix;
+    vector<Edge> BestInEdge;
+    int numVertices;
+
+    // ---------- Private Methods ---------- //
+    // ----- EDGE
+    bool _hasEdge(const int &from, const int &to);
+
+    // ----- métodos auxiliares de Edmonds Algorithm
+    void preprocess_edmonds(const int &root);
+    void contracting_stage(const int &root);
+    void expanding_stage(const int &root);
+    pair<int, int> BestEdge(const int &vertex);
+    void contract_cycle(vector<int>& cycle);
+    bool dfs(int v, const vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& stack, vector<int>& cycle);
 public:
     // ------ CONSTRUCTORS ----- //
     GraphMatrix(int numVertices);
@@ -32,168 +57,93 @@ public:
     bool remove_vertex(const int &vertex);
 
     // ------ OTHERS ------ //
+    // --- find cycles
     vector<int> findCycles();
-    void contract(const vector<int>& ciclo);
+    // ------ ALGORITMOS DE ARBORENCÊNCIA ------ //
+    void edmonds_algorithm(const int &root);
+
     // ------ DEBUG ------ //
     void print();
 
+
 };
 
-// --- PRIVATE FUNCTIONS
-
-inline bool GraphMatrix::dfs(int v, vector<bool>& visitado, vector<bool>& pilhaRecursiva, vector<int>& parent, stack<int>& ciclo){
-    visitado[v] = true;
-    pilhaRecursiva[v] = true;
-
-    for (int i = 0; i < numVertices; ++i) {
-        if (adjMatrix[v][i] != INT_MAX) {
-            if (!visitado[i]) {
-                parent[i] = v;
-                if (dfs(i, visitado, pilhaRecursiva, parent, ciclo)) return true;
-            } else if (pilhaRecursiva[i]) {
-                ciclo.push(i);
-                int u = v;
-                while (u != i) {
-                    ciclo.push(u);
-                    u = parent[u];
-                }
-                ciclo.push(i);
-                return true;
-            }
-        }
-    }
-
-    pilhaRecursiva[v] = false;
-    return false;
-}
-
 // ------ CONSTRUCTORS ----- //
+
 inline GraphMatrix::GraphMatrix(int numVertices){
     this->numVertices = numVertices;
-    adjMatrix.resize(numVertices, vector<int>(numVertices, INT_MAX));
+    this->adjMatrix.resize(numVertices, vector<Edge>(numVertices, Edge()));
+    this->BestInEdge.resize(numVertices, Edge());
 }
 
 // ------ EDGES ------//
-inline bool GraphMatrix::viable_edge(const int &from, const int &to) {
+
+inline bool GraphMatrix::viable_edge(const int &from, const int &to){
     return has_vertex(from) && has_vertex(to);
 }
-
-inline bool GraphMatrix::has_edge(const int &from, const int &to) {
-    return viable_edge(from, to) ? adjMatrix[from][to] != INT_MAX : false;
+inline bool GraphMatrix::has_edge(const int &from, const int &to){
+    return viable_edge(from, to) ? adjMatrix[from][to].score != INT_MAX : false;
 }
-
-inline bool GraphMatrix::_hasEdge(const int &from, const int &to) {
-    return adjMatrix[from][to] != INT_MAX;
-}
-
-inline bool GraphMatrix::add_edge(const int &from, const int &to, const int &weight = 1) {
+inline bool GraphMatrix::add_edge(const int &from, const int &to, const int &weight){
     if (viable_edge(from, to) && !_hasEdge(from, to)) {
-        adjMatrix[from][to] = weight;
+        adjMatrix[from][to].source = from;
+        adjMatrix[from][to].target = to;
+        adjMatrix[from][to].score = weight;
         return true;
     }
     return false;
 }
-
-inline bool GraphMatrix::remove_edge(const int &from, const int &to) {
+inline bool GraphMatrix::remove_edge(const int &from, const int &to){
     if (has_edge(from, to)) {
-        adjMatrix[from][to] = INT_MAX;
+        adjMatrix[from][to].source = -1;
+        adjMatrix[from][to].target = -1;
+        adjMatrix[from][to].score = INT_MAX;
         return true;
     }
     return false;
+    
 }
 
 // ------ VERTEX ------ //
-
-inline bool GraphMatrix::has_vertex(const int &vertex) {
+inline bool GraphMatrix::has_vertex(const int &vertex){
     return vertex >= 0 && vertex < numVertices;
 }
 
-inline void GraphMatrix::add_vertex(const int &newVertexCount) {
-        numVertices += newVertexCount;
-        for (auto& row : adjMatrix) {
-            row.resize(numVertices, INT_MAX);
-        }
-        for (int i = 0; i < newVertexCount; ++i) {
-            adjMatrix.push_back(vector<int>(numVertices, INT_MAX));
-        }
+inline void GraphMatrix::add_vertex(const int &newVertexCount){
+    numVertices += newVertexCount;
+    for(auto &row : adjMatrix){
+        row.resize(numVertices, Edge());
+    }
+    for (int i = 0; i < newVertexCount; ++i) {
+        adjMatrix.push_back(vector<Edge>(numVertices, Edge()));
+    }
 }
 
-inline bool GraphMatrix::remove_vertex(const int &vertex) {
-    if (vertex < 0 || vertex >= numVertices) {
+inline bool GraphMatrix::remove_vertex(const int &vertex){
+    if (!has_vertex(vertex)) {
         cout << "Vertice invalido!" << endl;
         return false;
     }
 
-    // Remove a linha x
-    adjMatrix.erase(adjMatrix.begin() + vertex);
+    // Remove a linha x 
+    adjMatrix.erase(adjMatrix.begin() + vertex);    
 
     // Remove a coluna x de cada linha restante
     for (auto& row : adjMatrix) {
         row.erase(row.begin() + vertex);
     }
-
     // Diminui o número de vértices
     numVertices--;
     return true;
 }
+// ------ ALGORITMOS DE ARBORENCÊNCIA ------ //
 
-// ------ OTHER FUNCTIONS ------ //
-
-inline vector<int> GraphMatrix::findCycles(){
-    vector<bool> visitado(numVertices, false);
-    vector<bool> pilhaRecursiva(numVertices, false);
-    vector<int> parent(numVertices, -1);
-    stack<int> ciclo;
-
-    for (int i = 0; i < numVertices; ++i) {
-        if (!visitado[i]) {
-            if (dfs(i, visitado, pilhaRecursiva, parent, ciclo)) {
-                vector<int> cicloVec;
-                while (!ciclo.empty()) {
-                    cicloVec.push_back(ciclo.top());
-                    ciclo.pop();
-                }
-                return cicloVec;
-            }
-        }
-    }
-    return {};
-}
-
-
-inline void GraphMatrix::contract(const vector<int>& ciclo){
-    
-    if (ciclo.empty())
-    {
-        return;
-    }
-    
-    // Criando super-vértice
-    int superVertice = numVertices++;
-    adjMatrix.resize(numVertices, vector<int>(numVertices, INT_MAX));
-    for (auto& row : adjMatrix) {
-        row.resize(numVertices, INT_MAX);
-    }
-
-    // Redirecionando arestas de/para o ciclo para o super-vértice
-    for (int i = 0; i < numVertices; ++i) {
-        for (int v : ciclo) {
-            if (i != superVertice && adjMatrix[i][v] != INT_MAX) {
-                adjMatrix[i][superVertice] = adjMatrix[i][v];
-                adjMatrix[i][v] = INT_MAX;
-            }
-            if (i != superVertice && adjMatrix[v][i] != INT_MAX) {
-                adjMatrix[superVertice][i] = adjMatrix[v][i];
-                adjMatrix[v][i] = INT_MAX;
-            }
-        }
-    }
-
-    // Removendo as arestas internas do ciclo
-    for (int i = (int)ciclo.size()-2; i >= 0 ; --i) {
-        remove_vertex(ciclo.at(i));
-    }
-    cout << endl;
+// algoritmo implementado baseado na apresentação de Sam Thompson
+// pela universidade de Washington, CSE 490u em 22 de fevereiro de 2017
+//segue o link da apresentação: https://courses.cs.washington.edu/courses/cse490u/17wi/slides/CLE.pdf
+inline void GraphMatrix::edmonds_algorithm(const int &root){
+    preprocess_edmonds(root);
+    contracting_stage(root);
 }
 
 // ------ DEBUG ------ //
@@ -207,11 +157,175 @@ inline void GraphMatrix::print(){
     for (int i = 0; i < numVertices; ++i) {
         cout << i << " ";
         for (int j = 0; j < numVertices; ++j) {
-            if (adjMatrix[i][j] == INT_MAX)
+            if (adjMatrix[i][j].score == INT_MAX)
+            {
                 cout << "0 ";
-            else
-                cout << adjMatrix[i][j] << " ";
+            }
+            else cout << adjMatrix[i][j].score << " ";
         }
         cout << endl;
     }
+}
+
+
+
+// ---------- Private Methods ---------- //
+// ----- EDGES
+inline bool GraphMatrix::_hasEdge(const int &from, const int &to) {
+    return adjMatrix[from][to].score != INT_MAX;
+}
+
+// ----- métodos auxiliares de Edmonds Algorithm
+inline void GraphMatrix::preprocess_edmonds(const int &root){
+    //remove todas as arestas de entrada do vértice raiz (root)
+    for (int u = 0; u < numVertices; u++)
+    {
+        if(adjMatrix[u][root].score == INT_MAX) continue;
+        adjMatrix[u][root].score = INT_MAX;
+    }
+}
+
+inline void GraphMatrix::contracting_stage(const int &root){
+    pair<int, int > bestEdges;
+
+    for (int v = 0; v < numVertices; v++)
+    {
+        // 1. para cada vértice v que não seja a raiz(root), 
+        //selecione a aresta de menor peso e coloque em BestInEdge[v]
+        if(v == root) continue;
+        bestEdges = BestEdge(v);
+        BestInEdge[v].source = bestEdges.first;
+        BestInEdge[v].target = v;
+        BestInEdge[v].score = bestEdges.second;
+
+        // 2. a cada seleção, verifique se não gerou ciclos
+        vector<int> cycle = findCycles();
+        if (!cycle.empty())// 3. se gerar ciclo, contraia, o nó para Vc
+        {
+            cout << "ciclo encontrado" << endl;
+            for(const auto &u : cycle) cout << u << " ";
+            cout << endl;
+
+            contract_cycle(cycle);
+        }
+    }
+    
+}
+
+inline void GraphMatrix::expanding_stage(const int &root){
+
+}
+
+inline pair<int, int> GraphMatrix::BestEdge(const int &vertex){
+
+    pair<int, int> bestEdge = {-1, INT_MAX};
+    for (int v = 0; v < numVertices; v++)
+    {
+        if (adjMatrix[v][vertex].score < bestEdge.second){
+            bestEdge.first = v;
+            bestEdge.second = adjMatrix[v][vertex].score;
+        }
+        
+    }
+
+    return bestEdge;
+    
+}
+inline void GraphMatrix::contract_cycle(vector<int>& cycle) {
+    int n = numVertices++;
+    int superVertex = n; // Escolhendo o primeiro vértice do ciclo como supervértice
+    adjMatrix.resize(numVertices, vector<Edge>(numVertices, Edge()));
+    BestInEdge.resize(numVertices, Edge());
+
+    // Inicializando um vetor para marcar os vértices do ciclo
+    vector<bool> isInCycle(n, false);
+    for (int v : cycle) {
+        isInCycle[v] = true;
+    }
+
+    // Para cada vertice u em C, e para cada aresta e de entrada de fora do ciclo para u
+    for(const auto &u : cycle){
+        for (int v = 0; v < numVertices; v++)
+        {
+            if (adjMatrix[v][u].score != INT_MAX)
+            { 
+                // coloque e.kicksOut = BestInEdge[u]
+                adjMatrix[v][u].kicksOut.push_back(BestInEdge[u]);
+                // atualize e.score para ser e.score - e.kicksOut.score
+                adjMatrix[v][u].score -= adjMatrix[v][u].kicksOut.back().score;
+            }
+            
+        }
+        
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (!isInCycle[i]) {
+            // Para cada vértice fora do ciclo, redirecione as arestas para o supervértice
+            for (int v : cycle) {
+                if (adjMatrix[i][v].score != INT_MAX) {
+                    adjMatrix[i][superVertex].score = adjMatrix[i][v].score;
+                    adjMatrix[i][v].score = INT_MAX;
+                }
+                if (adjMatrix[v][i].score != INT_MAX) {
+                    adjMatrix[superVertex][i].score = adjMatrix[v][i].score;
+                    adjMatrix[v][i].score = INT_MAX;
+                }
+            }
+        }
+    }
+    
+
+    // Removendo arestas internas do ciclo
+    for (int i = 0; i < cycle.size(); ++i) {
+        for (int j = 0; j < cycle.size(); ++j) {
+            adjMatrix[cycle[i]][cycle[j]].score = INT_MAX;
+        }
+    }
+}
+
+
+inline vector<int> GraphMatrix::findCycles() {
+    vector<vector<int>> graph;
+    graph.resize(numVertices);
+    for (const auto& edge : BestInEdge) {
+        if (edge.source >= 0 && edge.target >= 0)
+        {
+            graph[edge.target].push_back(edge.source);
+        }
+        
+    }
+
+    vector<bool> visited(numVertices, false), stack(numVertices, false);
+    vector<int> cycle;
+
+    for (int i = 0; i < numVertices; ++i) {
+        if (!visited[i] && dfs(i, graph, visited, stack, cycle)) {
+            break;
+        }
+    }
+
+    return cycle;
+}
+
+inline bool GraphMatrix::dfs(int v, const vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& stack, vector<int>& cycle) {
+    if (!visited[v]) {
+        visited[v] = true;
+        stack[v] = true;
+
+        for (int u : graph[v]) {
+            if (!visited[u] && dfs(u, graph, visited, stack, cycle)) {
+                if (cycle.empty() || stack[cycle.back()]) { // Checa se ainda estamos no ciclo
+                    cycle.push_back(u); // Adiciona o vértice ao ciclo
+                }
+                return true;
+            } else if (stack[u]) {
+                cycle.push_back(u); // Adiciona o vértice ao ciclo
+                return true;
+            }
+        }
+    }
+
+    stack[v] = false;
+    return false;
 }
--- graph/main.cpp
+++ graph/main.cpp
@@ -1,6 +1,6 @@
-#include "GraphList.hpp"
 #include "GraphMatrix.hpp"
 #include <iostream>
+#include <set>
 #include <vector>
 #include <utility> // para pair
 using namespace std;
@@ -10,27 +10,21 @@ int main() {
     cout << "------ Matriz de adjacencia ------ " << endl;
     GraphMatrix g(4);
 
-    g.add_edge(0,1,1);
+    g.print();
+
+    cout << "--------------------------------"<< endl;
+
+    g.add_edge(0,1,10);
     g.add_edge(1,2,1);
-    g.add_edge(2,3,1);
-    g.add_edge(3,1,1);
-    
-    g.print();
+    g.add_edge(2,3,2);
+    g.add_edge(3,1,3);
 
-    vector<int> gc = g.findCycles();
-    cout << "--------------------------------"<< endl;
-    for(int i : gc){
-        cout << i << " ";
-    }
-    cout << endl;
-
-    cout << "--------------------------------"<< endl;
-    GraphMatrix cg = g;
-    cg.contract(gc);
 
     g.print();
     cout << "--------------------------------"<< endl;
-    cg.print();
-
+    // cout << "---------------- aaa ----------------"<< endl;
+    // GraphMatrix cg = g;
+    g.edmonds_algorithm(0);
+    //g.print();
     return 0;
 }
